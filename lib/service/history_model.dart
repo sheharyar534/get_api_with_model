9ede0e210aaeef30e6021280185723b2b5ca01a2
sheharyar <analyst7@ugspk.com>
2024-10-18 18:24:38 +0500

    toJson has been added just for myself

--- lib/service/history_model.dart
+++ lib/service/history_model.dart
@@ -1,22 +1,31 @@
-
-
 import 'dart:convert';
 
-HistoryModel historyModelFromJson(String str){
-return HistoryModel.fromjson(jsonDecode(str));
+HistoryModel historyModelFromJson(String str) {
+  return HistoryModel.fromjson(jsonDecode(str));
 }
+
+String historyModelToJson(HistoryModel data) => json.encode(data.toJson());
 
 class HistoryModel {
   final int status;
   final List<HistoryDataModel> data;
   HistoryModel({required this.status, required this.data});
+
+  //from json
   factory HistoryModel.fromjson(Map<String, dynamic> json) {
     return HistoryModel(
-      status: json["status"],
-      data: List<HistoryDataModel>.from(
-        json["data"].map((x) => HistoryDataModel.fromjson(x)),
-      )
-    );
+        status: json["status"],
+        data: List<HistoryDataModel>.from(
+          json["data"].map((x) => HistoryDataModel.fromjson(x)),
+        ));
+  }
+
+  //to json
+  Map<String, dynamic> toJson() {
+    return {
+      "status": status,
+      "data": List<dynamic>.from(data.map((x) => toJson()))
+    };
   }
 }
 
@@ -27,9 +36,12 @@ class HistoryDataModel {
   final double? farePrice;
 
   HistoryDataModel(
-      {required this.pickuplocation,required this.droplocation,required this.distance, this.farePrice});
+      {required this.pickuplocation,
+      required this.droplocation,
+      required this.distance,
+      required this.farePrice});
 
-  //from json
+  //from json method
   factory HistoryDataModel.fromjson(Map<String, dynamic> json) {
     return HistoryDataModel(
       pickuplocation: json["pickup_location"],
@@ -38,4 +50,10 @@ class HistoryDataModel {
       farePrice: json['fare_price'],
     );
   }
+  //to jeson
+  Map<String, dynamic> toJson() => {
+        "pickup_location": pickuplocation,
+        "drop_location": droplocation,
+        "fare_price": farePrice,
+      };
 }
